dbe7fed863bdb3e562eac65ac45dad05a786fa2d
= <kamen.hristov.98@gmail.com>
2019-04-14 23:00:37 +0300

    added gateway event in map.py and fixed left direction

--- map.py
+++ map.py
@@ -3,11 +3,12 @@ import os
 
 class Map:
     LEVELS_PATH = 'Levels'
+    LEVELS = sorted(os.listdir(LEVELS_PATH))
     DIRECTIONS = {
         'up': (-1, 0),
         'down': (1, 0),
         'right': (0, 1),
-        'left': (-1, 0)
+        'left': (0, -1)
     }
     OBSTACLE = '#'
     TREASURE = 'T'
@@ -15,8 +16,8 @@ class Map:
     FREE_SPACE = '.'
     GATEWAY = 'G'
 
-    def __init__(self, matrix):
-        self.map = matrix
+    def __init__(self):
+        self.map = Map.from_file(Map.LEVELS.pop(0))
 
     def __str__(self):
         # equivalent to print_map()
@@ -26,7 +27,6 @@ class Map:
                 result += str(cell)
             result += '\n'
         return result
-        # return '\n'.join([''.join(row) for row in self.map])
 
     def spawn(self, hero):
         for row in self.map:
@@ -36,53 +36,71 @@ class Map:
                     y = self.map.index(row)
                     x = row.index(cell)
                     self.hero.update_cords(y, x)
-                    self.map[y][x] = hero
-                return
-
+                    self.map[y][x] = self.hero
+                    return
 
     def move_hero(self, direction):
-        prev_y_hero, prev_x_hero  = self.hero.y_coord, self.hero.x_coord
-        new_direction = Map.DIRECTIONS.get(direction, (0 ,0))
+        prev_y_hero, prev_x_hero = self.hero.y_coord, self.hero.x_coord
+        new_direction = Map.DIRECTIONS.get(direction, (0, 0))
         new_y, new_x = new_direction[0] + prev_y_hero, new_direction[1] + prev_x_hero
         if new_x < 0 or new_y < 0:
+            print('Can\t move in that direction!')
             return False
         try:
             if self.map[new_y][new_x] == Map.FREE_SPACE:
-                self.map[prev_y_hero][prev_x_hero] = Map.FREE_SPACE
-                self.map[new_y][new_x] = self.hero
-                self.hero.update_cords(new_y, new_x)
+                self.update_hero(prev_y_hero, prev_x_hero, new_y, new_x)
                 return True
             elif self.map[new_y][new_x] == Map.OBSTACLE:
                 print('Obstacle in the way!')
                 return False
             elif self.map[new_y][new_x] == Map.TREASURE:
-                # TODO: implement treasuer event
-                return False
+                print('Treasure found!')
+                self.update_hero(prev_y_hero, prev_x_hero, new_y, new_x)
+                # TODO: implement treasure event
+                return True
             elif self.map[new_y][new_x] == Map.ENEMY:
-                # TODO: implement enemy event
+                # TODO: implement battle event
                 return False
             elif self.map[new_y][new_x] == Map.GATEWAY:
-                # TODO: implement gateway event
+                self.found_gateway()
                 return False
         except IndexError:
-            # print('Edge of the maze reached!')
+            print('Can\t move in that direction!')
             return False
 
+    def update_hero(self, prev_y, prev_x, new_y, new_x):
+        self.map[prev_y][prev_x] = Map.FREE_SPACE
+        self.map[new_y][new_x] = self.hero
+        self.hero.update_cords(new_y, new_x)
 
-    @classmethod
-    def from_file(cls):
-        with open(os.path.join(Map.LEVELS_PATH, 'level1.txt')) as f:
-            return cls([[cell for cell in row.strip()]
-                        for row in f.readlines()])
+    def found_gateway(self):
+        try:
+            self.map = Map.from_file(Map.LEVELS.pop(0))
+            self.hero.take_healing(self.hero.max_health)
+            self.hero.take_mana(self.hero.max_mana)
+            self.spawn(self.hero)
+            print("You have reached the end of the level!\nBrace yourself for the upcoming one..")
+        except IndexError:
+            print('Congratulations, you have reached the end ot the dungeon and vanquished all the pythons!\n'
+                  f'{self.hero.known_as()}, you are a TRUE hero!')
+            exit(0)
+
+    @staticmethod
+    def from_file(file):
+        with open(os.path.join(Map.LEVELS_PATH, file)) as f:
+            return [[cell for cell in row.strip()]
+                    for row in f.readlines()]
 
 
 if __name__ == '__main__':
     from sprites import Hero
-    m = Map.from_file()
+    print(Map.LEVELS)
+    m = Map()
     print(m)
-    h = Hero(name='Jon', title='Assasin', health=100, mana=125, mana_regeneration_rate=2)
+    h = Hero(name='Jon', title='Assassin', health=100, mana=125, mana_regeneration_rate=2)
     m.spawn(h)
-    print(m)
-    print(m.move_hero('right'))
-    print(m)
+    while True:
+        print(m)
+        pos = input('>')
+        m.move_hero(pos)
 
