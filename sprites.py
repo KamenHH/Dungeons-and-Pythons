32f73aee3e018e1e3cf38a42e88e648812694a80
= <kamen.hristov.98@gmail.com>
2019-04-15 15:06:17 +0300

    added attack methods and changed some attr to properties

--- sprites.py
+++ sprites.py
@@ -4,16 +4,11 @@ class Sprite:
         self._max_health = health
         self._curr_mana = mana
         self._max_mana = mana
-        # self._curr_position = curr_position
-
-    # def __repr__(self):
-    #     """For debugging.."""
-    #     return ', '.join([str((k, v)) for k, v in self.__dict__.items()])
 
     @property
     def x_coord(self):
         return self._x
-    
+
     @x_coord.setter
     def x_coord(self, coord):
         self._x = coord
@@ -21,7 +16,7 @@ class Sprite:
     @property
     def y_coord(self):
         return self._y
-    
+
     @y_coord.setter
     def y_coord(self, coord):
         self._y = coord
@@ -42,15 +37,21 @@ class Sprite:
     def get_mana(self):
         return self._curr_mana
 
+    def get_status(self):
+        return f'Health: {self._curr_health}/{self.max_health}; Mana: {self._curr_mana}/{self.max_mana}\n' \
+               f'{self.has_weapon()} - damage: {self.weapon.damage if self.has_weapon() else 0}\n' \
+               f'{self.has_spell()} - damage: {self.spell.damage if self.has_spell() else 0}'
+
     def is_alive(self):
         """Returns true if current health is greater than 0."""
         return self._curr_health > 0
 
-    def can_cast(self, spell_cost):
-        """Returns true if current mana is greater than 0."""
-        return self._curr_mana > spell_cost 
+    def can_cast(self):
+        """Returns true if current mana is greater than 0 and enough to cast the spell."""
+        spell = self.__dict__.get('spell', 0)
+        return spell != 0 and self.get_mana() >= spell.mana_cost
 
-    def take_damage(self, damage):
+    def lose_health(self, damage):
         """Decreases sprite's health based on the given damage factor."""
         remaining_health = self._curr_health - damage
         if remaining_health <= 0:
@@ -73,7 +74,7 @@ class Sprite:
         else:
             self._curr_mana = remaining_mana
 
-    def take_mana(self, restore_amount):
+    def increase_mana(self, restore_amount):
         """Restores sprite's mana, if new value doesn't exceed max_mana"""
         new_mana = self._curr_mana + restore_amount
         if new_mana >= self.max_mana:
@@ -82,31 +83,40 @@ class Sprite:
             self._curr_mana = new_mana
 
     def attack(self, by=None):
-        if self._curr_mana == 0:
-            damage, mana_cost = self.__dict__.get('weapon', 0).use()
+        if by == 'weapon':
+            return self.attack_by_weapon()
+        elif by == 'magic' or by == 'spell':
+            return self.attack_by_spell()
         else:
-            damage, mana_cost = self.__dict__.get(by, 0).use()
-            if mana_cost != 0:
-                self.spend_mana(mana_cost)
-        return damage
+            raise ValueError("No weapon provided")
+
+    def attack_points(self, by=None):
+        return self.attack(by)
+
+    def attack_by_weapon(self):
+        weapon = self.__dict__.get('weapon', 0)
+        if weapon == 0:
+            return 0
+        return weapon.damage
+
+    def attack_by_spell(self):
+        if self.can_cast():
+            return self.spell.damage
+        return 0
 
     def update_cords(self, new_y, new_x):
         self.y_coord = new_y
         self.x_coord = new_x
 
-    # def move(self, direction):
-    #     """Move sprite in one of 4 possible directions.
-    #         Adds, the coordinates of the new position to those of the current one."""
-    #     # direction tuple = (down & up <y-axis>, right & left <x-axis>)
-    #     directions = {'up': (-1, 0),
-    #                   'down': (1, 0),
-    #                   'right': (0, 1),
-    #                   'left': (0, -1)
-    #                   }
-    #     new_direction = directions.get(direction, (0, 0))
-    #     self.y_coord += new_direction[0]
-    #     self.x_coord += new_direction[1]
+    def distance_from(self, other):
+        return abs(self.y_coord - other.y_coord) \
+               + abs(self.x_coord - other.x_coord)
 
+    def has_weapon(self):
+        return getattr(self, 'weapon', None)
+
+    def has_spell(self):
+        return getattr(self, 'spell', None)
 
 
 class Hero(Sprite):
@@ -121,6 +131,10 @@ class Hero(Sprite):
 
     def __repr__(self):
         return str(self)
+
+    @property
+    def mana_regeneration_rate(self):
+        return self._mana_regeneration_rate
 
     def known_as(self):
         """Returns hero's name and title."""
@@ -139,7 +153,7 @@ class Enemy(Sprite):
     def __init__(self, health, mana, damage):
         super().__init__(health, mana)
         self._damage = damage
-    
+
     def __str__(self):
         return 'E'
 
@@ -150,21 +164,22 @@ class Enemy(Sprite):
     def damage(self):
         return self._damage
 
+    @classmethod
+    def from_json(cls, json_file):
+        import json
+        with open(json_file) as f:
+            enemy_data = Enemy.pick_random_enemy(json.load(f))
+        return cls(**enemy_data)
 
-if __name__ == '__main__':
-    h = Hero('Billy', 'Wizard', health=250, mana=100, mana_regeneration_rate=2)
-    print(h.__dict__)
-    # print(h.is_alive())
-    # print(h.known_as())
-    # import time
-    # while True:
-    #     h.take_healing(21)
-    #     print(h.get_health())
-    #     time.sleep(0.5)
-    #     print(h.is_alive())
-    h.x_coord = 10
-    print(h.x_coord)    
+    @staticmethod
+    def pick_random_enemy(enemies_data):
+        from random import choice
+        return choice(enemies_data)
 
-
+# if __name__ == '__main__':
+#     h = Hero('Billy', 'Wizard', health=250, mana=100, mana_regeneration_rate=2)
+#     e = Enemy.from_json('enemies.json')
+#     print(e.__dict__)
+#
 
 
