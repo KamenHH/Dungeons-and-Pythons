8e8d28ca909a9d7005790fea28f5d3b17bf138a0
= <kamen.hristov.98@gmail.com>
2019-04-15 15:03:24 +0300

    changed collectables.py

--- collectables.py
+++ collectables.py
@@ -2,33 +2,33 @@ import json
 import random
 import sys
 
+
 class TreasureChest:
-    """Used to load and parse the collectables.json, 
+    """Used to load and parse the collectables.json,
     pick a random item and create an object instance from the parsed data."""
+
     # treasures = parse_json()
     def __init__(self):
-        
         self.treasure = __class__.parse_object()
 
     def __str__(self):
-        return 
+        return
 
     @classmethod
     def parse_object(cls):
         class_info = cls._pick_item()
         class_name = class_info.pop('type')
         return getattr(sys.modules[__name__], class_name)(**class_info)
-    
+
     @classmethod
-    def parse_json(cls):  
+    def parse_json(cls):
         """Gets the json data."""
         with open('treasures.json') as jf:
             cls.treasures = json.load(jf)
-    
+
     @classmethod
     def _pick_item(cls):
         return random.choice(cls.treasures)
-
 
 
 class MeansOfAttack:
@@ -37,6 +37,7 @@ class MeansOfAttack:
     damage: the damage that the type of attack deals on a single use,
     mele attack: scope = 1, ranged attack: scope > 1
     """
+
     def __init__(self, name, damage, scope):
         self._name = name
         self._damage = damage
@@ -45,7 +46,8 @@ class MeansOfAttack:
     def __str__(self):
         return f'{self._name}'
 
-    def get_damage(self):
+    @property
+    def damage(self):
         return self._damage
 
     def use(self):
@@ -54,14 +56,16 @@ class MeansOfAttack:
 
 class Weapon(MeansOfAttack):
     """Weapon class, can be ranged or mele."""
+
     def __init__(self, name, damage, scope):
         super().__init__(name, damage, scope)
 
 
 class Spell(MeansOfAttack):
     """Spell class, mostly ranged, but can be mele
-    mana cost: the amount of points that will be taken from the 
+    mana cost: the amount of points that will be taken from the
     hero's mana for a single use of the spell."""
+
     def __init__(self, name, damage, scope, mana_cost):
         super().__init__(name, damage, scope)
         self._mana_cost = mana_cost
@@ -72,6 +76,7 @@ class Spell(MeansOfAttack):
 
 class Potion:
     """Restores some amount of health, mana, or both, to the hero in play."""
+
     def __init__(self, name, health_amount, mana_amount):
         self._name = name
         self._health_amount = health_amount
@@ -83,10 +88,10 @@ class Potion:
     def use(self):
         return self._health_amount, self._mana_amount
 
-if __name__ == "__main__":
-    TreasureChest.parse_json()
-    tc = TreasureChest()
-    # t.parse_object()
-    # print(Treasure.treasures)
-    # print(t.treasure)
-    print(tc.treasure.__dict__)
+# if __name__ == "__main__":
+#     TreasureChest.parse_json()
+#     tc = TreasureChest()
+#     # t.parse_object()
+#     # print(Treasure.treasures)
+#     # print(t.treasure)
+#     print(tc.treasure.__dict__)
